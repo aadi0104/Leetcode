6fc870cad9063d0671702b3cc9313fc57725a6ce
Aadil Ahmad <aadill0104@gmail.com>
2024-11-13 21:19:20 +0530

    First Commit

---
+++ src/LeetCode/Arranging_Coins_441.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class Arranging_Coins_441 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int ans = arrangeCoins(n);
+		System.out.print(ans);
+	}
+	
+	public static int arrangeCoins(int n) {
+        long low = 0;
+        long n_long = (long)n;
+        long high = n_long;
+        while(low <= high){
+            long mid = low + (high - low)/2;
+            if (mid * (mid + 1)/2 <= n_long){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return (int) low - 1;
+    }
+
+}
