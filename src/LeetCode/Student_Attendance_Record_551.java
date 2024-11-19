ca1f5d330c89ec3906ede154cd2a42844456ca68
Aadil Ahmad <aadill0104@gmail.com>
2024-11-19 13:18:19 +0530

    Student Attendance Record

---
+++ src/LeetCode/Student_Attendance_Record_551.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class Student_Attendance_Record_551 {
+
+	public static void main(String[] args) {
+		String s = "PPALLL";
+		boolean ans = Student_Attendance_Record(s);
+		System.out.print(ans);
+	}
+
+	public static boolean Student_Attendance_Record(String s) {
+		int absent = 0;
+		int late = 0;
+		int count = 0;
+		while (count < s.length()) {
+			if (s.charAt(count) == 'A') {
+				absent++;
+			}
+			if (s.charAt(count) == 'L') {
+				late++;
+			} else {
+				late = 0;
+			}
+			count++;
+			if (absent == 2 || late == 3) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
