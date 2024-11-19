c4a7cbc562fd63ae7e29cb1167cdf1ecb15ed708
Aadil Ahmad <aadill0104@gmail.com>
2024-11-19 18:37:34 +0530

    Fizz Buzz

---
+++ src/LeetCode/Fizz_Buzz_412.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.*;
+
+public class Fizz_Buzz_412 {
+
+	public static void main(String[] args) {
+		int n = 15;
+		List<String> list = Fizz_Buzz(n);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i) + ", ");
+		}
+	}
+
+	public static List<String> Fizz_Buzz(int n) {
+		List<String> list = new ArrayList<>();
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				list.add("FizzBuzz");
+			} else if (i % 3 == 0) {
+				list.add("Fizz");
+			} else if (i % 5 == 0) {
+				list.add("Buzz");
+			} else {
+				list.add(Integer.toString(i));
+			}
+		}
+		return list;
+	}
+
+}
