c2f3c70b37512e3a96e39107c92bc758c1134f23
Aadil Ahmad <aadill0104@gmail.com>
2024-11-19 16:58:39 +0530

    Jewels And Stones

---
+++ src/LeetCode/Jewels_And_Stones_771.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class Jewels_And_Stones_771 {
+
+	public static void main(String[] args) {
+		String jewels = "aA";
+		String stones = "aAAbbbb";
+		int ans = Jewels_And_Stones(jewels, stones);
+		System.out.print(ans);
+	}
+
+	public static int Jewels_And_Stones(String jewels, String stones) {
+		int count = 0;
+		for (int i = 0; i < jewels.length(); i++) {
+			if (stones.indexOf(jewels.charAt(i)) != -1) {
+				stones = Remove_Character_At_Index(stones, stones.indexOf(jewels.charAt(i)));
+				i--;
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static String Remove_Character_At_Index(String s, int i) {
+		char [] ch = s.toCharArray();
+		ch[i] = ' ';
+		String str = new String(ch);
+		return str;
+	}
+
+}
