549a688b7f562809dc366fb8b878346074c7ee49
Aadil Ahmad <aadill0104@gmail.com>
2024-11-30 20:49:23 +0530

    Generate Parantheses

---
+++ src/LeetCode/Generate_Parantheses_22.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+import java.util.*;
+
+public class Generate_Parantheses_22 {
+
+	public static void main(String[] args) {
+		int n = 3;
+		List<String> s = new ArrayList<>();
+		Generate_Parantheses(n, 0, 0, "", s);
+		System.out.print(s);
+	}
+
+	public static void Generate_Parantheses(int n, int open, int closed, String ans, List<String> s) {
+		if (open == n && closed == n) {
+			s.add(ans);
+			return;
+		}
+		if (open > n || closed > open) {
+			return;
+		}
+		Generate_Parantheses(n, open + 1, closed, ans + "(", s);
+		Generate_Parantheses(n, open, closed + 1, ans + ")", s);
+	}
+
+}
