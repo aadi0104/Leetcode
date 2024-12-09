6478448342d4c0634208db14af30c0d8ff80c56f
Aadil Ahmad <aadill0104@gmail.com>
2024-12-09 20:18:19 +0530

    Combinations

---
+++ src/LeetCode/Combinations_77.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+import java.util.*;
+
+public class Combinations_77 {
+
+	public static void main(String[] args) {
+		int n = 4;
+		int k = 2;
+		List<Integer> temp = new ArrayList<>();
+		List<List<Integer>> ans = new ArrayList<>();
+		Combinations(n, k, temp, ans, 1);
+		System.out.println(ans);
+	}
+
+	public static void Combinations(int n, int k, List<Integer> temp, List<List<Integer>> ans, int idx) {
+		if (temp.size() == k) {
+			ans.add(new ArrayList<>(temp));
+			return;
+		}
+		for (int i = idx; i <= n; i++) {
+			temp.add(i);
+			Combinations(n, k, temp, ans, i + 1);
+			temp.remove(temp.size() - 1);
+		}
+	}
+
+}
