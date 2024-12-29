5b993d4b636097df14d6a4b808a158079fa17976
Aadil Ahmad <aadill0104@gmail.com>
2024-12-29 20:49:59 +0530

    Construct Smallest Number From DI String

---
+++ src/LeetCode/Construct_Smallest_Number_From_DI_String_2375.java
@@ -0,0 +1,34 @@
+package LeetCode;
+
+import java.util.*;
+
+public class Construct_Smallest_Number_From_DI_String_2375 {
+
+	public static void main(String[] args) {
+		String pattern = "IIIDIDDD";
+		String ans = Construct_Smallest_Number_From_DI_String(pattern);
+		System.out.print(ans);
+	}
+
+	public static String Construct_Smallest_Number_From_DI_String(String pattern) {
+		int[] arr = new int[pattern.length() + 1];
+		Stack<Integer> st = new Stack<>();
+		int count = 1;
+		for (int i = 0; i <= pattern.length(); i++) {
+			if (i == pattern.length() || pattern.charAt(i) == 'I') {
+				arr[i] = count++;
+				while (!st.isEmpty()) {
+					arr[st.pop()] = count++;
+				}
+			} else {
+				st.push(i);
+			}
+		}
+		String ans = "";
+		for (int i : arr) {
+			ans += i;
+		}
+		return ans;
+	}
+
+}
