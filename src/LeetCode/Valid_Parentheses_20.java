b7ac0a323506db600d65759fc08f65b3c3f7ddd0
Aadil Ahmad <aadill0104@gmail.com>
2024-12-29 20:26:43 +0530

    Valid Parentheses

---
+++ src/LeetCode/Valid_Parentheses_20.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+import java.util.*;
+
+public class Valid_Parentheses_20 {
+
+	public static void main(String[] args) {
+		String s = "([])";
+		System.out.println(Valid_Parentheses(s));
+	}
+
+	public static boolean Valid_Parentheses(String s) {
+		Stack<Character> st = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch == '(' || ch == '{' || ch == '[') {
+				st.push(ch);
+			} else {
+				if (st.isEmpty()) {
+					return false;
+				} else if (ch == ')' && st.peek() == '(') {
+					st.pop();
+				} else if (ch == '}' && st.peek() == '{') {
+					st.pop();
+				} else if (ch == ']' && st.peek() == '[') {
+					st.pop();
+				} else {
+					return false;
+				}
+			}
+		}
+		if (st.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+
+}
