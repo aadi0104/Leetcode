6300d655f81ae283e0d888961ab437d2f3b9ec05
Aadil Ahmad <aadill0104@gmail.com>
2025-01-12 18:50:47 +0530

    Linked List Cycle

---
+++ src/LeetCode/Linked_List_Cycle_141.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+public class Linked_List_Cycle_141 {
+
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode();
+		Linked_List_Cycle(head);
+	}
+
+	public static boolean Linked_List_Cycle(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (fast == slow) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
