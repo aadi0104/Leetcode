cbd89a799a13b3afeaa3688480d4862d6c0e0e5e
Aadil Ahmad <aadill0104@gmail.com>
2025-01-12 18:45:13 +0530

    Middle Of The Linked List

---
+++ src/LeetCode/Middle_Of_The_Linked_List_876.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+public class Middle_Of_The_Linked_List_876 {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode();
+		Middle_Of_The_Linked_List(head);
+	}
+
+	public static ListNode Middle_Of_The_Linked_List(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
+}
