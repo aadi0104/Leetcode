fe86b2b6048ef82d60e879b04ea70ea308ebb19f
Aadil Ahmad <aadill0104@gmail.com>
2025-01-13 14:28:15 +0530

    Reverse Linked List

---
+++ src/LeetCode/Reverse_Linked_List_206.java
@@ -0,0 +1,45 @@
+package LeetCode;
+
+import Class29.Linked_List.Node;
+
+public class Reverse_Linked_List_206 {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode();
+		Reverse_Linked_List(head);
+	}
+
+	public static ListNode Reverse_Linked_List(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		ListNode newhead = Reverse_Linked_List(head.next);
+		AddEnd(head);
+        return newhead;
+
+	}
+
+	public static void AddEnd(ListNode head) {
+        ListNode front = head.next;
+		front.next = head;
+		head.next = null;
+	}
+
+}
