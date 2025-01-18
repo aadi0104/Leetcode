7c5d55212b5f27fd3123c5caf2aed45a2587c055
Aadil Ahmad <aadill0104@gmail.com>
2025-01-18 20:13:53 +0530

    Merge Two Sorted Lists

---
+++ src/LeetCode/Merge_Two_Sorted_Lists_21.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+public class Merge_Two_Sorted_Lists_21 {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static void main(String[] args) {
+		ListNode list1 = new ListNode();
+		ListNode list2 = new ListNode();
+		Merge_Two_Sorted_Lists(list1, list2);
+	}
+
+	public static ListNode Merge_Two_Sorted_Lists(ListNode list1, ListNode list2) {
+		ListNode dummy = new ListNode();
+		ListNode temp = dummy;
+		while (list1 != null && list2 != null) {
+			if (list1.val < list2.val) {
+				temp.next = list1;
+				list1 = list1.next;
+			} else {
+				temp.next = list2;
+				list2 = list2.next;
+			}
+			temp = temp.next;
+		}
+		return dummy.next;
+	}
+
+}
