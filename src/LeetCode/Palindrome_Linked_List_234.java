03de3a67ccdb76523cdfcc30621e5603ca846e3e
Aadil Ahmad <aadill0104@gmail.com>
2025-01-30 20:51:33 +0530

    Palindrome Linked List

--- src/LeetCode/Palindrome_Linked_List_234.java
+++ src/LeetCode/Palindrome_Linked_List_234.java
@@ -21,7 +21,7 @@ public class Palindrome_Linked_List_234 {
 		}
 	}
 
-	public static void main(String[] args) {
+	public void main(String[] args) {
 		ListNode head = new ListNode();
 		Palindrome_Linked_List(head);
 	}
