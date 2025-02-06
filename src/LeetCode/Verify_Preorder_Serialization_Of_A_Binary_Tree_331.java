e80d16ed78e4a18552e2a4a650e42b79d0fb8e0e
Aadil Ahmad <aadill0104@gmail.com>
2025-02-06 17:44:34 +0530

    Verify Preorder Serialization Of A Binary Tree

---
+++ src/LeetCode/Verify_Preorder_Serialization_Of_A_Binary_Tree_331.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+public class Verify_Preorder_Serialization_Of_A_Binary_Tree_331 {
+
+	public boolean main(String[] args) {
+		String preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#";
+		return Verify_Preorder_Serialization_Of_A_Binary_Tree(preorder);
+	}
+
+	public boolean Verify_Preorder_Serialization_Of_A_Binary_Tree(String preorder) {
+		int vac = 1;
+		String[] ch = preorder.split(",");
+		for (int i = 0; i < ch.length; i++) {
+			vac--;
+			if (vac < 0) {
+				return false;
+			}
+			if (!ch[i].equals("#")) {
+				vac += 2;
+			}
+		}
+		return (vac == 0);
+	}
+
+}
