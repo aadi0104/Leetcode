4edb2be123598cf0fd84654630fc9b1441021d7f
Aadil Ahmad <aadill0104@gmail.com>
2025-02-26 21:46:10 +0530

    Longest Substring Without Repeating Characters

---
+++ src/LeetCode/Longest_Substring_Without_Repeating_Characters_3.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.*;
+
+public class Longest_Substring_Without_Repeating_Characters_3 {
+
+	public static void main(String[] args) {
+		String s = "pwwkew";
+		int ans = Longest_Substring_Without_Repeating_Characters(s);
+		System.out.print(ans);
+	}
+
+	public static int Longest_Substring_Without_Repeating_Characters(String s) {
+		int ans = 0;
+		int st = 0;
+		int en = 0;
+		HashSet<Character> set = new HashSet<>();
+		while (en < s.length()) {
+            char currentChar = s.charAt(en);
+            while (set.contains(currentChar)) {
+                set.remove(s.charAt(st));
+                st++;
+            }
+            set.add(currentChar);
+            ans = Math.max(ans, en - st + 1);
+            en++;
+        }
+		return ans;
+	}
+
+}
