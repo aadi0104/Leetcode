10f099a6f1e84ca68eb1e25b461be006ae224968
aadi0104 <aadill0104@gmail.com>
2025-03-18 16:39:13 +0530

    Climbing Stairs

---
+++ src/LeetCode/Climbing_Stairs_70.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class Climbing_Stairs_70 {
+
+	public static void main(String[] args) {
+		int n = 3;
+		int[] dp = new int[n + 1];
+		int ans = Climbing_Stairs(n, 1, dp);
+		System.out.print(ans);
+	}
+
+	public static int Climbing_Stairs(int n, int i, int[] dp) {
+		if (i >= n) {
+			return 1;
+		}
+		if (dp[i] != 0) {
+			return dp[i];
+		}
+		int onestep = Climbing_Stairs(n, i + 1, dp);
+		int twostep = Climbing_Stairs(n, i + 2, dp);
+		return dp[i] = onestep + twostep;
+	}
+
+}
