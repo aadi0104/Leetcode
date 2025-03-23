f4790d78adf710aa379560c42cd78b96d3b5ee53
aadi0104 <aadill0104@gmail.com>
2025-03-23 20:09:07 +0530

    Implement Trie (Prefix Tree)

---
+++ src/LeetCode/Implement_Trie_208.java
@@ -0,0 +1,64 @@
+package LeetCode;
+
+import java.util.*;
+
+public class Implement_Trie_208 {
+
+	class Node {
+		char ch;
+		HashMap<Character, Node> child = new HashMap<>();
+		boolean isTerminated;
+	}
+
+	private Node root;
+
+	class Trie {
+
+		public Trie() {
+			root = new Node();
+		}
+
+		public void insert(String word) {
+			Node curr = root;
+			for (int i = 0; i < word.length(); i++) {
+				char ch = word.charAt(i);
+				if (curr.child.containsKey(ch)) {
+					curr = curr.child.get(ch);
+				} else {
+					Node nn = new Node();
+					nn.ch = ch;
+					curr.child.put(ch, nn);
+					curr = nn;
+				}
+			}
+			curr.isTerminated = true;
+		}
+
+		public boolean search(String word) {
+			Node curr = root;
+			for (int i = 0; i < word.length(); i++) {
+				char ch = word.charAt(i);
+				if (curr.child.containsKey(ch)) {
+					curr = curr.child.get(ch);
+				} else {
+					return false;
+				}
+			}
+			return curr.isTerminated;
+		}
+
+		public boolean startsWith(String prefix) {
+			Node curr = root;
+			for (int i = 0; i < prefix.length(); i++) {
+				char ch = prefix.charAt(i);
+				if (curr.child.containsKey(ch)) {
+					curr = curr.child.get(ch);
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+}
